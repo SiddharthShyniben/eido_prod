8fad3e2d845bc106c75a6ccdfdff7b6da3db3b66
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-07-06 12:41:56 +0530

    Add login button to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,7 @@
                 <p><strong>Say goodbye to long tutorial videos and boring text
                     tutorials.</strong><br>Eido is a dynamic approch to tutorials which will make tutorials easy to understand and fun.</p>
                 <Button href="/signup">Start now</Button>
+                <Button href="/login" kind="secondary">Log in</Button>
             </Column>
         </Row>
     </Grid>
