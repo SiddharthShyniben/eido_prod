ea71db15f76f45a0e4b8a590730d244d1fe10b4c
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-07-06 12:32:47 +0530

    Add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {Content, Grid, Row, Column, FluidForm, TextInput, PasswordInput, Button} from "carbon-components-svelte";
+
+    let email, password;
+
+    let email_invalid = false;
+    let password_invalid = false;
+
+    $: email_invalid = email && !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i.test(email);
+    $: password_invalid = password && password.length < 8;
+
+    function submit() {
+        alert(`${email}:${password}`)
+    }
+</script>
+
+<Content>
+    <Grid>
+        <Row>
+            <Column>
+                <h1>Log in</h1>
+                <FluidForm on:submit={submit}>
+                    <TextInput
+                        labelText="Email"
+                        placeholder="nix@the_gods.com" required
+                        bind:value={email}
+                        invalid={email_invalid}
+                        invalidText="You must provide a valid email"
+                    />
+                    <PasswordInput
+                        required
+                        type="password"
+                        labelText="Password"
+                        placeholder="••••••••••"
+                        bind:value={password}
+                        invalid={password_invalid}
+                        invalidText="Your password must be at least 8 characters long"
+                    />
+                    <Button disabled={!(email && password)} type="submit">Log in</Button>
+                </FluidForm>
+            </Column>
+        </Row>
+    </Grid>
+</Content>
