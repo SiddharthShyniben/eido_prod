74f80db08b73381c8428fe480bfed11498075d8b
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-07-08 21:59:20 +0530

    Implement admin page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,17 +1,31 @@
 
 <script>
     import {user} from "../../stores";
+    import {listRef} from "../../firebase";
+
     import {Loading, ClickableTile} from "carbon-components-svelte";
+    import {listAll} from "firebase/storage";
 
     let loading = true;
-
     let currentUser;
+    let tuts = [];
+
     user.subscribe(u => {
         if (u) {
             currentUser = u;
             loading = false;
+            main();
+        } else {
+            // if (browser) goto('/login');
         }
-    })
+    });
+
+    async function main() {
+        const tutorials = await listAll(listRef);
+        tuts = tutorials.items;
+    }
+
+    const formatTitle = t => [...t.fullPath.slice(4, -3)].map((c, i) => i === 0 ? c.toUpperCase() : c).join('')
 </script>
 
 {#if loading}
@@ -20,13 +34,13 @@
     <h1><strong>Hi, {currentUser.displayName}</strong></h1>
     <p>Explore our tutorials:</p>
 
-    <ClickableTile href="https://www.carbondesignsystem.com/">
-        Carbon Design System
-    </ClickableTile>
-    <ClickableTile href="https://www.carbondesignsystem.com/">
-        Carbon Design System
-    </ClickableTile>
-    <ClickableTile href="https://www.carbondesignsystem.com/">
-        Carbon Design System
-    </ClickableTile>
+    {#if tuts.length > 0}
+        {#each tuts as tut}
+            <ClickableTile href="/tutorial/{tut.fullPath.slice(4, -3)}">
+                <strong>{formatTitle(tut)}</strong>
+            </ClickableTile>
+        {/each}
+    {:else}
+        <Loading />
+    {/if}
 {/if}
