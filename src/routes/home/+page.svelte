d0ad837c35b8a9803849f83942e55e39419c5c2d
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-10-19 20:45:03 +0530

    Add accordions

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,12 +3,13 @@
     import {user} from "../../stores";
     import {listRef} from "../../firebase";
 
-    import {Loading, ClickableTile} from "carbon-components-svelte";
+    import {Loading, ClickableTile, Accordion, AccordionItem} from "carbon-components-svelte";
     import {listAll} from "firebase/storage";
 
     let loading = true;
     let currentUser;
     let tuts = [];
+    let cat = {};
 
     user.subscribe(u => {
         if (u) {
@@ -24,9 +25,15 @@
         const tutorials = await listAll(listRef);
         tuts = tutorials.items.map(t => ({
             title: t.name.split('---')[1].split('-')[0],
+            category: t.name.split('---')[1].split('-')[1].slice(0, -3),
             fullPath: t.fullPath,
             routerLink: t.name.slice(0, -3)
         }))
+
+        tuts.forEach(t => {
+            cat[t.category] ??= []
+            cat[t.category].push(t)
+        })
     }
 </script>
 
@@ -34,14 +41,20 @@
     <Loading />
 {:else}
     <h1><strong>Hi, {currentUser.displayName}</strong></h1>
-    <p>Explore our tutorials:</p>
+    <p>Explore our tutorials:</p><br><br>
 
-    {#if tuts.length > 0}
-        {#each tuts as tut}
-            <ClickableTile href="/tutorials/{tut.routerLink}">
-                <strong>{tut.title}</strong>
-            </ClickableTile>
-        {/each}
+    {#if Object.keys(cat).length > 0}
+        <Accordion align="start">
+            {#each Object.entries(cat) as [category, tuts]}
+                <AccordionItem title={category}>
+                    {#each tuts as tut}
+                        <ClickableTile href="/tutorials/{tut.routerLink}">
+                            <strong>{tut.title}</strong>
+                        </ClickableTile>
+                    {/each}
+                </AccordionItem>
+            {/each}
+        </Accordion>
     {:else}
         <Loading />
     {/if}
